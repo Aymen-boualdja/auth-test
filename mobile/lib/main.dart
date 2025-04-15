dc262b372b63d6dcd693d5747550d4272b7793f8
Aymen-boualdja <a.boualdja@esi-sba.dz>
2025-04-15 20:26:41 +0100

    logout ui

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:kidergarten/pages/login_page.dart';
-import 'package:kidergarten/pages/signup_page.dart';
+import 'package:kidergarten/pages/ParentProfile.dart';
 import 'package:kidergarten/pages/testpage.dart';
 import 'package:kidergarten/pages/welcome_page.dart';
-import 'package:kidergarten/services/api_service.dart';
 
 void main() {
-
   runApp(const MainApp());
 }
 
@@ -15,8 +12,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WelcomePage(),
+    return MaterialApp(
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
